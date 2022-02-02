a1677096dd590dbc3da2b342f27a195082914659
Fuad-Bracu <83465032+Fuad-Bracu@users.noreply.github.com>
2022-02-03 02:50:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* ============================== typing animation ============================ */
+
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Engineer", "Developer", "cyber security analyst", "Digital Marketer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Engineer", "Developer", "cyber security analyst", "Digital Marketer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
